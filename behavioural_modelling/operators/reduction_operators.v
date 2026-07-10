65ec4f67ba7290a5a6d6dbfd3efb0bec8120e436
Mehul Gupta <mehulgupta.mg@yahoo.com>
2026-07-10 15:47:10 +0530

    Update reduction_operators.v

--- behavioural_modelling/operators/reduction_operators.v
+++ behavioural_modelling/operators/reduction_operators.v
@@ -1,5 +1,12 @@
 module reduction_operators;
   reg [3:0] a;
+
+  reg red_and; //&
+  reg red_or;
+  reg red_nor;
+  reg red_nand;
+  reg red_xor;
+  reg red_xnor;
   initial
     begin
       a = 4'b1010;
