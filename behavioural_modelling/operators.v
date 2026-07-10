3e7c16288868c198dc264581333f13a81dd7cfba
Mehul Gupta <mehulgupta.mg@yahoo.com>
2026-07-10 15:19:14 +0530

    Create operators.v

---
+++ behavioural_modelling/operators.v
@@ -0,0 +1,50 @@
+module operators;
+  reg [3:0] a;
+  reg [3:0] b;
+
+  reg gt;
+  reg lt;
+  reg gtoeq;
+  reg ltoeq;
+
+  reg eq;
+  reg neq;
+  reg ceq;
+  reg cneq;
+
+  initial
+    begin
+      a = 4'b1011;
+      b = 4'b1100;
+
+      gt = a > b;
+      $display("the value of gt is %b",gt);
+      
+      lt = a < b;
+      $display("the value of lt is %b",lt);
+
+      a = 4'b101x
+      b = 4'b1100   //this input will give don't care
+
+      gtoeq = a >= b;
+      $display("the value of gtoeq is %b",gtoeq);
+      
+      ltoeq = a <= b;
+      $display("the value of ltoeq is %b",ltoeq);
+
+      eq = a==b;
+      $display("the value of eq is %b",eq);
+
+      neq = a!=b;
+      $display("the value of neq is %b",neq);
+
+      a = 4'b101x;
+      b = 4'b1100;
+      //case equality can compare these values
+      ceq = a===b;
+      $display("the value of ceq is %b",ceq);
+      
+      cneq = a!==b;
+      $display("the value of cneq is %b",cneq);
+    end
+endmodule
