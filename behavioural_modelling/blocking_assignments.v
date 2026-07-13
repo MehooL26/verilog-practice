cc86778354973b716f96b159704d9b7f23b55c39
Mehul Gupta <mehulgupta.mg@yahoo.com>
2026-07-14 00:29:12 +0530

    Update blocking_assignments.v

--- behavioural_modelling/blocking_assignments.v
+++ behavioural_modelling/blocking_assignments.v
@@ -1,3 +1,19 @@
 // all variables on left side should be of reg type
 
 // Inter assignment delay : 
+// case - 1:
+always 
+  begin
+    #1 a = 0;  //after one time unit value of a will be 0
+    #2 b = 1;  //after two time unit, total 3 time unit, value of b will be 1
+  end
+//these delays are not synthesisable, only used for testbench purpose
+
+// case - 2:
+always
+  begin
+    #1;
+    a = 0;
+    #2;
+    b = 1;
+  end
