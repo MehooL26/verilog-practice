1631da15fadc14d039aade6174b6282300f4cbda
Mehul Gupta <mehulgupta.mg@yahoo.com>
2026-07-15 16:50:47 +0530

    Update priority_encoder.v

--- behavioural_modelling/priority_encoder.v
+++ behavioural_modelling/priority_encoder.v
@@ -7,9 +7,9 @@ module priority_encoder(
     begin
       casex(d)
         4'b0001 : y = 2'b00;
-        4'b0010 : y = 2'b01;
-        4'b0100 : y = 2'b10;
-        4'b1000 : y = 2'b11;
+        4'b001x : y = 2'b01;
+        4'b01xx : y = 2'b10;
+        4'b1xxx : y = 2'b11;
         default : y = 'bx;
       endcase
     end
