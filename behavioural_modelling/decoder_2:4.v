cfc4e268816c062877b985f1ff9a3b8cb1c03339
Mehul Gupta <mehulgupta.mg@yahoo.com>
2026-07-15 16:36:14 +0530

    Create decoder_2:4.v

---
+++ behavioural_modelling/decoder_2:4.v
@@ -0,0 +1,20 @@
+module decoder_2_4(
+  input [1:0] d,
+  output reg [3:0] y;
+);
+
+  always@(*)
+    begin
+      if (d == 2'b00)
+        y = 4'b0001;
+      else
+        if (d == 2'b01)
+        y = 4'b0010;
+      else 
+        if (d == 2'b10)
+        y = 4'b0100;
+      else
+        if (d == 2'b11)
+        y = 4'b1000;
+    end
+endmodule
