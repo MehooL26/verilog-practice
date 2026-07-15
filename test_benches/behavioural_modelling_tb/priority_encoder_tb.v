cd4ce243a1da37721383911579e8a25bc0b5e7af
Mehul Gupta <mehulgupta.mg@yahoo.com>
2026-07-15 16:50:28 +0530

    Create priority_encoder_tb.v

---
+++ test_benches/behavioural_modelling_tb/priority_encoder_tb.v
@@ -0,0 +1,22 @@
+module priority_encoder_tb;
+  reg [3:0] d_tb;
+  wire [1:0 ]y;
+
+  priority_encoder dut(d,y);
+
+  initial
+    begin
+      {d} = 0;
+    end
+
+  initial
+    begin
+      d = 4'b0001;
+      #1;
+      d = 4'b0010;
+      #1;
+      d = 4'b0100;
+      #1;
+      d = 4'b1000;
+    end
+endmodule
