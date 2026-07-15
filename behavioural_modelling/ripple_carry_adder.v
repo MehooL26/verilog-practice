22aaacaec5470d93c701b5cf448d13bf57b2bb23
Mehul Gupta <mehulgupta.mg@yahoo.com>
2026-07-15 12:57:31 +0530

    Update ripple_carry_adder.v

--- behavioural_modelling/ripple_carry_adder.v
+++ behavioural_modelling/ripple_carry_adder.v
@@ -4,5 +4,10 @@ module ripple_carry_adder(
   output [3:0] sum,
   output carry
 );
+  wire w1, w2, w3;
+  full_adder fa1(a[0], b[0], c_in, sum[0], w1);
+  full_adder fa2(a[1], b[1], w1, sum[1], w2);
+  full_adder fa3(a[2], b[2], w2, sum[2], w3);
+  full_adder fa4(a[3], b[3], w3, sum[3], carry);
   
 endmodule
