0306846561701561f17af9e8908ee56e9a5aa436
Mehul Gupta <mehulgupta.mg@yahoo.com>
2026-07-19 23:32:37 +0530

    Update t_latch.v

--- behavioural_modelling/sequential_circuits/t_latch.v
+++ behavioural_modelling/sequential_circuits/t_latch.v
@@ -2,4 +2,24 @@ module t_latch(
   input enb, rst, t_in, 
   output reg q, qbar
 );
+
+  always @ (enb)
+    begin
+      if(enb)
+        if(rst)
+          begin
+            q <= 1'b0;
+            qbar <= 1'b1;
+          end
+      else if (t_in == 0)
+        begin
+          q <= q;
+          qbar <= qbar;
+        end
+      else if (t_in == 1'b1)
+        begin
+          q <= ~q;
+          qbar <= ~qbar;
+        end
+    end
 endmodule
