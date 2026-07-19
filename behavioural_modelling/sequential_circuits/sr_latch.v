e2c4a6fae8467f76f93dfa60345a67e275781859
Mehul Gupta <mehulgupta.mg@yahoo.com>
2026-07-19 23:20:24 +0530

    Update sr_latch.v

--- behavioural_modelling/sequential_circuits/sr_latch.v
+++ behavioural_modelling/sequential_circuits/sr_latch.v
@@ -39,5 +39,41 @@ module sr_latch(
       q <= 'bx;
       qbar <= 'bx;
     end
+
+
+  //asynchronous reset
+  always@(enb,rst)
+    begin
+      if (rst) 
+        begin
+          q <= 1'b0;
+          qbar <= 1'b1;
+        end
+      if (enb)
+        begin
+          //hold condition 
+          if(s==0 && r==0)
+            begin
+              q <= q;
+              qbar <= qbar;
+            end
+          //reset condition
+          else if (s==0 && r==1)
+            begin
+              q <= 1'b0;
+              qbar <= 1'b1;
+            end
+          //set condition
+          else if (s==1 && r==0)
+            begin 
+              q <= 1;
+              qbar <= 1'b0;
+            end
+          //invalid condition
+          else if (s==1 && r==1)
+            begin
+              q <= 'bx;
+              qbar <= 'bx;
+            end
   
 endmodule
