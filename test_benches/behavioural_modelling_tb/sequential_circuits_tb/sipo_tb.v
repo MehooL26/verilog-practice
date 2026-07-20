5815b24cdc7e21bc07573d196cd62bf39b3b85b9
Mehul Gupta <mehulgupta.mg@yahoo.com>
2026-07-20 14:03:20 +0530

    Create sipo_tb.v

---
+++ test_benches/behavioural_modelling_tb/sequential_circuits_tb/sipo_tb.v
@@ -0,0 +1,31 @@
+module sipo;
+  reg clk,rst,enb,s_in;
+  wire p_out;
+
+  sipo dut(clk,rst,enb,s_in,p_out);
+
+  initial
+    begin
+      {clk,rst,enb,s_in} = 0;
+    end
+
+  always #5 clk = ~clk;
+
+  initial
+    begin
+      rst = 1;
+      #10;
+      rst = 0;
+      #10;
+      s_in = 1'b1;
+      #10;
+      s_in = 1'b1;
+      #10;
+      s_in = 1'b0;
+      #10;
+      s_in = 1'b0;
+      #30;
+      enb = 1;
+    end
+endmodule
+      
