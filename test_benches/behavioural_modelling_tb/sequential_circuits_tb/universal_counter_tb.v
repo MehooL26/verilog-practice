0907f170ccfbe7569a00a2d56c5157332e917501
Mehul Gupta <mehulgupta.mg@yahoo.com>
2026-07-31 16:36:35 +0530

    Update universal_counter_tb.v

--- test_benches/behavioural_modelling_tb/sequential_circuits_tb/universal_counter_tb.v
+++ test_benches/behavioural_modelling_tb/sequential_circuits_tb/universal_counter_tb.v
@@ -1,16 +1,17 @@
 module universal_counter_tb;
   reg clk,reset,enb;
   reg [1:0] mode;
+  reg up_down;
   wire mod_2_count;
   wire [1:0] mod_4_count;
   wire [2:0] mod_8_count;
   wire [3:0] mod_16_count;
 
-  universal_counter dut(clk,reset,enb,mode,mod_2_count,mod_4_count,mod_8_count,mod_16_count);
+  universal_counter dut(clk,reset,enb,mode,up_down,mod_2_count,mod_4_count,mod_8_count,mod_16_count);
 
   initial
     begin
-      {clk,reset,enb,mode} = 0;
+      {clk,reset,enb,mode,up_down} = 0;
     end
 
   always #5 clk = ~clk;
