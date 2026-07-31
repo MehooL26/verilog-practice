237fc6828ae45f2e2f3dae2c44b507cb2ece79c4
Mehul Gupta <mehulgupta.mg@yahoo.com>
2026-07-31 16:35:48 +0530

    Update universal_counter.v

--- behavioural_modelling/sequential_circuits/universal_counter.v
+++ behavioural_modelling/sequential_circuits/universal_counter.v
@@ -1,6 +1,7 @@
 module universal_counter(
   input clk,reset,enb,
   input [1:0] mode,
+  input up_down,
   output reg mod_2_count,
   output reg [1:0] mod_4_count,
   output reg [2:0] mod_8_count,
@@ -13,8 +14,12 @@ module universal_counter(
     begin
       if(rst) 
         internal_counter <= 0;
-      else if(enb)
+      else if(enb && up_down)
         internal_counter <= internal_counter + 1'b1;
+      else if(enb && up_down==0)
+        internal_counter <= internal_counter - 1'b1;
+      else 
+        internal_counter <= internal_counter;
     end
 
   always @ (posedge clk)
@@ -35,6 +40,13 @@ module universal_counter(
         2'b11 :
           begin
             mod_16_count <= internal_counter[3:0];
+          end
+        default :
+          begin
+            mod_2_count <= 0;
+            mod_4_count <= 0;
+            mod_8_count <= 0;
+            mod_16_count <= 0;
           end
       endcase
     end
