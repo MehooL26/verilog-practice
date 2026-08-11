b5f944b5b0a0f73eba5da65317440b3120d4a776
Mehul Gupta <mehulgupta.mg@yahoo.com>
2026-08-11 22:06:59 +0530

    Update mod_5_counter_tb.v

--- test_benches/behavioural_modelling_tb/sequential_circuits_tb/mod_5_counter_tb.v
+++ test_benches/behavioural_modelling_tb/sequential_circuits_tb/mod_5_counter_tb.v
@@ -2,7 +2,7 @@ module mod_5_counter_tb;
   reg clk,reset,enb;
   wire [2:0] count;
 
-  mod_5_counet(clk,reset,enb,count);
+  mod_5_counter(clk,reset,enb,count);
 
   always #5 clk = ~clk;
 
@@ -17,4 +17,7 @@ module mod_5_counter_tb;
       #10;
       reset = 0;
       enable = 1;
+
+      #100;
+      enable = 0;
     end
